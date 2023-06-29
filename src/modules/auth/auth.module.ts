b3c5008b4f08a66da96dbc79fad5dfbdddcb7255
Vasili Belashkov <boss.belashkov@mail.ru>
2023-06-29 14:16:12 +0300

    fixed imports in auth.module.ts

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { PassportModule } from '@nestjs/passport';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 
+import { UserModule } from 'modules/user/user.module';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UserModule } from 'modules/user/user.module';
 import { LocalStrategy } from './strategy/local.strategy';
 import { JwtRefreshTokenStrategy } from './strategy/jwt-refresh-token.strategy';
 import { JwtStrategy } from './strategy/jwt.strategy';
