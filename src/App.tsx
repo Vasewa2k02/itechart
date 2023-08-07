bff319c6f050a048e1ddd56401b7500970a0b4a1
Vasili Belashkov <boss.belashkov@mail.ru>
2023-08-07 14:00:20 +0300

    configured husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css';
-import logo from './logo.svg';
+import React from 'react';
 
-function App(): any {
+function App(): JSX.Element {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
